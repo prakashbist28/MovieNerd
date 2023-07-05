3236da6d03a47f2e6d2a0d4e6fd9410f72f4ff18
Prakash Bist <prakashcena540@gmail.com>
2023-07-06 02:19:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'https://movienerd-front.onrender.com/', 
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
